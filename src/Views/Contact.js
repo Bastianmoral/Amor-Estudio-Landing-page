7dc0a495c3da0c21c1a2aa38b6c3cf6e99fa3284
bastianmoral <bastihc@gmail.com>
2022-07-26 17:36:58 -0400

    trying to fix responsivity

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 
 export default function Contact() {
-  return <div class="body-contact">
+  return <div 
+          className="body-contact"
+          id="contact"
+
+        >
   <svg
-    class="vector-lines-contact"
+    className="vector-lines-contact"
     width="1829"
     height="670"
     xmlns="http://www.w3.org/2000/svg"
@@ -17,37 +21,39 @@ export default function Contact() {
   </svg>
   <Link to="/">
   <svg
-    class="vector-exit"
+    className="vector-exit"
     width="160"
     height="161"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path stroke="#F1ECE9" d="m.354.646 159 159M159.354 1.354l-159 159" />
   </svg>
-  </Link> 
-  <div class="contact-title">
+  </Link>
+  <div 
+    className="container"> 
+  <div className="contact-title">
     haz las cosas con <b>amor</b>
   </div>
-  <div class="contact-text">
+  <div className="contact-text">
     Trabajemos juntos, escríbenos y conversemos sobre lo que necesitas
     <br />
-    <div class="email">hola@amorestudio.design</div>
+    <div className="email">hola@amorestudio.design</div>
   </div>
   <a
     href="https://www.instagram.com/amor.estgr/?hl=en"
-    class="ig-container-contact"
+    className="ig-container-contact"
   >
     Instagram
   </a>
   <a
     href="https://www.behance.net/gallery/113078441/amorestgr-Branding"
-    class="behance-container-contact"
+    className="behance-container-contact"
   >
     Behance
   </a>
-  {/* <img src="../skins/home/vector_1.png" class="vector-container" /> */}
-  <div class="brand-container-contact">Branding e Ilustración</div>
-    <Link to="/"class="paginator-container"> amor Estudio </Link> 
+  <div className="brand-container-contact">Branding e Ilustración</div>
+    <Link to="/"className="paginator-container"> amor Estudio </Link> 
+    </div>
 </div> 
 }
 
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,36 +3,36 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return  ( 
-  <div class="body-home">
-  <svg class="vector-arrow" width="54" height="214" xmlns="http://www.w3.org/2000/svg">
+  <div className="body-home" id="home">
+  <svg className="vector-arrow" width="54" height="214" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M28.333 3a2.667 2.667 0 1 0 5.334 0 2.667 2.667 0 0 0-5.334 0ZM.501 212.971a.5.5 0 0 0 .47.528l4.492.263a.5.5 0 1 0 .059-.998l-3.994-.234.234-3.993a.5.5 0 0 0-.998-.059L.5 212.971ZM30.556 3.23c10.801 20.854 22.276 56.373 21.57 95.098-.707 38.712-13.584 80.61-51.458 114.298l.664.748c38.126-33.912 51.083-76.098 51.793-115.027.71-38.916-10.815-74.598-21.681-95.577l-.888.46Z"
       fill="#181818" />
   </svg>
-  <svg class="vector-line" width="1829" height="670" xmlns="http://www.w3.org/2000/svg">
+  <svg className="vector-line" width="1829" height="670" xmlns="http://www.w3.org/2000/svg">
     <path stroke="#181818" d="M0 .5h1829M0 223.5h1829M0 446.5h1829M0 669.5h1829" />
   </svg>
-  <main class="container">
-    <div class="title-container">
+  <main className="container">
+    <div className="title-container">
         amor
       </div>
-    <div class="center-container">
+    <div className="center-container">
       estudio de diseño gráfico 
     </div>
-    <div class="construction-container">
+    <div className="construction-container">
       Nuestro sitio está en construcción, pero puedes ver nuestro trabajo en
    </div>  
-   <a href="https://www.instagram.com/amor.estgr/?hl=en" class="ig-container">Instagram</a>
-   <a href="https://www.behance.net/gallery/113078441/amorestgr-Branding" class="behance-container">Behance</a>
- {/* <!--   <img src="../skins/home/vector_1.png" class="vector-container" /> --> */}
-   <div class="brand-container" >
+   <a href="https://www.instagram.com/amor.estgr/?hl=en" className="ig-container">Instagram</a>
+   <a href="https://www.behance.net/gallery/113078441/amorestgr-Branding" className="behance-container">Behance</a>
+ {/* <!--   <img src="../skins/home/vector_1.png" className="vector-container" /> --> */}
+   <div className="brand-container" >
       Branding e Ilustración
   </div>
   <div
     id="gif-container"
-    className="gif-container"
+    classNameName="gif-container"
   >   </div>
-  <Link to="/Contact"class="paginator-container"> amor Estudio </Link>
+  <Link to="/Contact"className="paginator-container"> amor Estudio </Link>
 </main>
 
 </div>
